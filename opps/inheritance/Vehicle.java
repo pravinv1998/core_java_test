7cd42c151d57fddcdbc347b3f07a8cd89908885a
pravinv1998 <pravin.wargad1998@gmail.com>
2026-01-07 23:47:54 +0530

    opps - inheritance

--- opps/inheritance/Vehicle.java
+++ opps/inheritance/Vehicle.java
@@ -1,5 +1,28 @@
 package opps.inheritance;
 
 public class Vehicle {
+    private String brand;
+    private int speed;
+
+    public Vehicle(String brand, int speed) {
+        this.brand = brand;
+        this.speed = speed;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
 
 }
