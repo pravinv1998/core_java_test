91c8afde7dcdf4eb1efd56509904634ab6ab36c9
pravinv1998 <pravin.wargad1998@gmail.com>
2026-01-14 02:44:37 +0530

    opps - abstraction

---
+++ opps/abstraction/Car.java
@@ -0,0 +1,29 @@
+package opps.abstraction;
+
+public class Car extends Vehicle {
+    private int doors;
+
+    public Car(String brand, int speed, int doors) {
+        super(brand, speed);
+        this.doors = doors;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public void start() {
+        System.out.println("Start Car");
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Brand: " + getBrand() + ", Speed: " + getSpeed() + "Km/H. & doors " + doors);
+    }
+
+}
